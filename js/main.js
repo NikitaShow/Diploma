0dbf12f6cb3bada994d7f066b73399e5e07d1f38
Nikita <nikita.show@mail.ru>
2020-06-11 21:20:34 +0300

    begin slider footer

--- js/main.js
+++ js/main.js
@@ -4,18 +4,30 @@ $(document).ready(function () {
     // Optional parameters
     loop: true,
     pagination: {
-      el: this.querySelector ('.swiper-pagination'),
+      el: '.pag-1',
       type: 'fraction',
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: this.querySelector('.swiper-button-next'),
+      prevEl: this.querySelector('.swiper-button-prev'),
     }
   });
   var mySwiper = new Swiper('.swiper-container', {
     loop: true,
   });
+  var mySwiper = new Swiper ('.swiper-container1', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.pag-2',
+      type: 'fraction',  },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
 });
+
 
 
 
