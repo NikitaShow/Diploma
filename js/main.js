252de28e455c465ac6cfe33b6774daf9bf09e145
Nikita <nikita.show@mail.ru>
2020-06-14 15:11:27 +0300

    add last slider and adaptive for few section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $('.menu__burger').click(function(event){
+    $('.menu__burger, .burger__span, .menu__nav').toggleClass('active'),
+    $('body').toggleClass('lock')
+  });
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
@@ -28,12 +32,17 @@ $(document).ready(function () {
   });
   var mySwiper = new Swiper ('.swiper-container2', {
     // Optional parameters
-    slidesPerView: 2,
-    spaceBetween: 90,
+    slidesPerView: 1,
     loop: true,
     navigation: {
       nextEl: this.querySelector('.swiper-button-next3'),
       prevEl: this.querySelector('.swiper-button-prev3'),
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 90
+      }
     }
   });
 });
